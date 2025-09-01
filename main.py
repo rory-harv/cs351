a891ba0c48fcf296f5d32fd38ab46cd32af576eb
rory <victoria.harvey1@icloud.com>
2025-09-01 10:59:03 -0700

    updated merge

--- main.py
+++ main.py
@@ -11,10 +11,35 @@ def selection_sort(arr):
     pass
 
 def merge_sort(arr):
-    # TODO: Implement
-    pass
 
+    if len(arr) == 1:
+        return arr
+
+    mid = len(arr) // 2
+    
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+
+    return result
 
 if __name__ == "__main__":
-    time_algorithm()
+    array = [6,2,4,8]
+    print(merge_sort(array))
     
